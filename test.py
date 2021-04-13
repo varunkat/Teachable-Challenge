b477e894ff43f37f005a4a1bc49aa0d682b49e8e
varun <varun.sky56@gmail.com>
2021-04-12 23:07:13 -0600

    Finished SQUARE pattern

--- test.py
+++ test.py
@@ -4,14 +4,14 @@ test = [(1, [1, 1]), (2, [1, 2]), (3, [1, 3]), (4, [1, 4]), (5, [2, 1]), (6, [2,
 neighbors = []
 #-----------------------CROSS---------------------------------------
 x = 3
-y = 2
+y = 3
 m = 2
 count = 0
 tot1 = y+m
 tot2 = x+m
 T = (x,y)
 neighbors.append(T)
-for i in test:
+'''for i in test:
     if i[1][0] == x and i[1][1] == y:
         continue
     if i[1][0] == x:
@@ -24,7 +24,17 @@ for i in test:
         for k in range(1, tot2+1):
             if i[1][0] == k:
                 T1 = (k, y)
-                neighbors.append(T1)
+                neighbors.append(T1)'''
+
+#------------------------SQUARE-----------------------------------------
+for cord in test:
+    for i in range(x-m, x + m + 1):
+        for j in range(y-m, y + m + 1):
+            if cord[1][0] == i and cord[1][1] == j:
+                T = (i,j)
+                neighbors.append(T)
+
+
 
 
 print(neighbors)
