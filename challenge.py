baf22a030af64eafef3f4e81ff3db4a42687a179
varun <varun.sky56@gmail.com>
2021-04-13 17:16:39 -0600

    Finished Diamond pattern and task 2

--- challenge.py
+++ challenge.py
@@ -1,39 +1,92 @@
-import numpy as np
 
 grid = []
 neighbors = []
 
 class NeighboringNodes():
 
-    def __init__(self, size, debug):
+    def __init__(self, size, debug:bool):
         self.size = int(size)
         self.debug = debug
 
+
     def create_grid(self):
-        grid1 = [[row,col] for row in range(1, self.size+1) for col in range(1, self.size+1)]
+        grid1 = [[row,col] for row in range(1, self.size + 1) for col in range(1, self.size + 1)]
 
         for i in enumerate(grid1, 1):
             grid.append(i)
-        print(grid)
+        #print(grid)
 
-        if self.debug == 'True' or self.debug == 'true':
-            for i in grid:
-                feature = (i[1][0], i[1][1], i[0])
+        if self.debug == True:
+            for node in grid:
+                feature = (node[1][0], node[1][1], node[0])
                 print(feature)
 
-        #grid= np.zeros((self.size, self.size))
-        #return grid
 
     def return_cords(self, index):
-        self.index= index
-        for i in grid:
-            if i[0] == index:
-                T = (i[1][0], i[1][1])
+        for node in grid:
+            if node[0] == index:
+                T = (node[1][0], node[1][1])
         return T
 #------------------------END OF TASK 1-----------------------------------------
+    def patterns(self, x, y, i, m, type):
+        if x is not None and y is not None and i is not None:
+            print("expected either x & y or i")
+        else:
+            T = (x,y)
+            neighbors.append(T)
+            for node in grid:
+                if type == "CROSS":
+                    if node[1][0] == x and node[1][1] == y:
+                        continue
+                    if node[1][0] == x:
+                        for j in range(y - m, y + m + 1):
+                            if node[1][1] == j:
+                                T = (x, j)
+                                neighbors.append(T)
 
+                    if node[1][1] == y:
+                        for k in range(x - m, x + m + 1):
+                            if node[1][0] == k:
+                                T1 = (k, y)
+                                neighbors.append(T1)
 
-obj = NeighboringNodes(4, 'false')
-obj.create_grid()
-cords = obj.return_cords(3)
+                elif type == "SQUARE":
+                    for i in range(x - m, x + m + 1):
+                        for j in range(y - m, y + m + 1):
+                            if node[1][0] == x and node[1][1] == y:
+                                continue
+                            if node[1][0] == i and node[1][1] == j:
+                                T = (i,j)
+                                neighbors.append(T)
+
+                elif type == "DIAMOND":
+                    for v in range(0, m + 1):
+                        if node[1][0] == x and node[1][1] == y:
+                            continue
+
+                        if node[1][0] == x - v:
+                            for j in range(y - m + v, y + m + 1 - v):
+                                if node[1][1] == j:
+                                    T = (x - v, j)
+                                    if T in neighbors:
+                                        continue
+                                    else:
+                                        neighbors.append(T)
+
+                        if node[1][0] == x + v:
+                            for j in range(y - m + v, y + m + 1 - v):
+                                if node[1][1] == j:
+                                    T = (x + v, j)
+                                    if T in neighbors:
+                                        continue
+                                    else:
+                                        neighbors.append(T)
+
+        return neighbors
+
+object = NeighboringNodes(5, False)
+object.create_grid()
+cords = object.return_cords(12)
+neighbor_nodes = object.patterns(x = 3, y = 3, i = None , m = 2, type = "DIAMOND")
 print(cords)
+print(neighbor_nodes)
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-test = [(1, [1, 1]), (2, [1, 2]), (3, [1, 3]), (4, [1, 4]), (5, [2, 1]), (6, [2, 2]), (7, [2, 3]), (8, [2, 4]), (9, [3, 1]), (10, [3, 2]), (11, [3, 3]), (12,
-[3, 4]), (13, [4, 1]), (14, [4, 2]), (15, [4, 3]), (16, [4, 4])]
+test = [(1, [1, 1]), (2, [1, 2]), (3, [1, 3]), (4, [1, 4]), (5, [1, 5]), (6, [2, 1]), (7, [2, 2]), (8, [2, 3]), (9, [2, 4]), (10, [2, 5]), (11, [3, 1]), (12,
+[3, 2]), (13, [3, 3]), (14, [3, 4]), (15, [3, 5]), (16, [4, 1]), (17, [4, 2]), (18, [4, 3]), (19, [4, 4]), (20, [4, 5]), (21, [5, 1]), (22, [5, 2]), (23, [5, 3]), (24, [5, 4]), (25, [5, 5])]
 
 neighbors = []
 #-----------------------CROSS---------------------------------------
@@ -11,28 +11,73 @@ tot1 = y+m
 tot2 = x+m
 T = (x,y)
 neighbors.append(T)
+for i in test:
+    for v in range(0, m + 1):
+        if i[1][0] == x and i[1][1] == y:
+            continue
+
+        if i[1][0] == x - v:
+            for j in range(y - m + v, tot1 + 1 - v):
+                if i[1][1] == j:
+                    T = (x - v, j)
+                    if T in neighbors:
+                        continue
+                    else:
+                        neighbors.append(T)
+
+        if i[1][0] == x + v:
+            for j in range(y - m + v, tot1 + 1 - v):
+                if i[1][1] == j:
+                    T = (x + v, j)
+                    if T in neighbors:
+                        continue
+                    else:
+                        neighbors.append(T)
+
+    '''if i[1][1] == y:
+        for k in range(x-m, tot2+1):
+            if i[1][0] == k:
+                T1 = (k, y)
+                neighbors.append(T1)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 '''for i in test:
     if i[1][0] == x and i[1][1] == y:
         continue
     if i[1][0] == x:
-        for j in range(1, tot1+1):
+        for j in range(y-m, tot1+1):
             if i[1][1] == j:
                 T = (x, j)
                 neighbors.append(T)
 
     if i[1][1] == y:
-        for k in range(1, tot2+1):
+        for k in range(x-m, tot2+1):
             if i[1][0] == k:
                 T1 = (k, y)
                 neighbors.append(T1)'''
 
 #------------------------SQUARE-----------------------------------------
-for cord in test:
+'''for cord in test:
     for i in range(x-m, x + m + 1):
         for j in range(y-m, y + m + 1):
             if cord[1][0] == i and cord[1][1] == j:
                 T = (i,j)
-                neighbors.append(T)
+                neighbors.append(T)'''
 
 
 
